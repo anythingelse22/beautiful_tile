2f5526d2f8841af0889740e1b4280ab06af5c4f0
anythingelse22 <48355032+anythingelse22@users.noreply.github.com>
2022-07-22 09:30:09 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const arr = {
+	// email
+	email: "random123@gmail.com",
+	// phone
+	phone: "8-800-555-35-35",
+}
+
+render_str()
+
+function render_str() {
+	const em = document.getElementById("email_id")
+	const ph = document.getElementById("phone_id")
+
+	em.innerText = arr.email
+	ph.innerText = arr.phone
+}
